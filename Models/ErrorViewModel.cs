02d7b4ce1228b432fefea4a32d737da9defd82db
Juan Orellana <juan.orellana2@catolica.edu.sv>
2025-05-19 22:12:47 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FrontEndTicketPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
