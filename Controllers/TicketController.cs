00cc5162ead330a6988febea0a01c4cc638a4176
Angel Magana <maganaangel342@gmail.com>
2025-05-22 09:24:19 -0600

    Vista CrearTicket diseño completada

--- Controllers/TicketController.cs
+++ Controllers/TicketController.cs
@@ -19,5 +19,31 @@ namespace FrontEndTicketPro.Controllers
             var tickets = await _http.GetFromJsonAsync<List<ticket>>("api/ticket");
             return View(tickets);
         }
+
+        //public async Task<IActionResult> CrearTicket()
+        //{
+        //    return View();
+        //}
+
+        //para las categorias 
+        public async Task<IActionResult> CrearTicket()
+        {
+            var categorias = new List<categoria_ticket>();
+            var response = await _http.GetAsync("/api/categoria");
+
+            if (response.IsSuccessStatusCode)
+            {
+                categorias = await response.Content.ReadFromJsonAsync<List<categoria_ticket>>();
+            }
+
+            var modelo = new CrearTicketViewModel
+            {
+                Ticket = new ticket(),
+                Categorias = categorias
+            };
+
+            return View(modelo); // 🔴 ESTA línea debe existir, no `View();`
+        }
+
     }
 }
