11aa53f56113ca0c1cf878e0e4a99e2371bed5ba
Angel Magana <maganaangel342@gmail.com>
2025-05-24 21:52:10 -0600

    Ticket y detalle ticket completados, solo falta el del archivo adjunto

---
+++ Models/Admin/TicketDetalleViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FrontEndTicketPro.Models.Admin
+{
+    public class TicketDetalleViewModel
+    {
+        public int IdTicket { get; set; }
+        public string Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Servicio { get; set; }
+        public string Descripcion { get; set; }
+        public string Categoria { get; set; }
+        public string Prioridad { get; set; }
+        public string Estado { get; set; }
+        public string ArchivoUrl { get; set; }
+
+        public string ClienteNombre { get; set; }
+        public string ClienteCorreo { get; set; }
+        public string ClienteTelefono { get; set; }
+    }
+
+}
