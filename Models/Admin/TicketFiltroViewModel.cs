791bf368918833e1bd3fecf7a6384f18383d6497
Estefany Sanabria <estefanysanabrias@gmail.com>
2025-05-26 21:17:47 -0600

    Gestión de usuarios y tickets casi completo

---
+++ Models/Admin/TicketFiltroViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FrontEndTicketPro.Models.Admin
+{
+    public class TicketFiltroViewModel
+    {
+        public int? IdTicket { get; set; }
+        public string BuscarTexto { get; set; }
+        public string Estado { get; set; }
+        public string Prioridad { get; set; }
+        public DateTime? FechaDesde { get; set; }
+        public DateTime? FechaHasta { get; set; }
+
+        public List<TablaTicketsInicio> TicketsFiltrados { get; set; }
+    }
+}
